85feb016b483ae5e7e7f83ae5799a1ddae7d492f
Shuvo Banarjee <168532196+Banarjee07@users.noreply.github.com>
2024-07-02 16:42:13 +0600

    Create footer.php

---
+++ Project-1-Library management System/footer.php
@@ -0,0 +1,11 @@
+   <section class="footer-section"> 
+        <div class="container"> 
+            <div class="row"> 
+                <div class="col-md-12"> 
+                   &copy; <?php echo date('Y');?> Online Library Management System <a 
+href="https://phpgurukul.com/" target="_blank"  > Designed by : Rakib hasan</a>  
+                </div> 
+ 
+</div> 
+</div> 
+</section>

408ed220798e09a76b19d3f4b8c4f577f0473547
Shuvo Banarjee <168532196+Banarjee07@users.noreply.github.com>
2024-07-02 16:44:41 +0600

    Create header.php

---
+++ Project-1-Library management System/header.php
@@ -0,0 +1,79 @@
+header.php 
+<div class="navbar navbar-inverse set-radius-zero"> 
+<div class="container"> 
+<div class="navbar-header"> 
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> 
+<span class="icon-bar"></span> 
+<span class="icon-bar"></span> 
+<span class="icon-bar"></span> 
+</button> 
+<a class="navbar-brand"> 
+<img src="assets/img/logo.png" /> 
+</a> 
+</div> 
+<div class="right-div"> 
+<a href="logout.php" class="btn btn-danger pull-right">LOG ME OUT</a> 
+</div> 
+</div> 
+</div> 
+<!-- LOGO HEADER END--> 
+<section class="menu-section"> 
+<div class="container"> 
+<div class="row "> 
+<div class="col-md-12"> 
+<div class="navbar-collapse collapse "> 
+<ul id="menu-top" class="nav navbar-nav navbar-right"> 
+<li><a href="dashboard.php" class="menu-top-active">DASHBOARD</a></li> 
+Categories <i 
+<li> 
+<a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown"> 
+class="fa fa-angle-down"></i></a> 
+<ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem"> 
+<li role="presentation"><a role="menuitem" tabindex="-1" href="add-category.php">Add 
+Category</a></li> 
+<li role="presentation"><a role="menuitem" tabindex="-1" 
+href="manage-categories.php">Manage Categories</a></li> 
+</ul> 
+</li> 
+<li> 
+<a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown"> Authors 
+<i 
+books.php">Manage 
+class="fa fa-angle-down"></i></a> 
+<ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem"> 
+<li role="presentation"><a role="menuitem" tabindex="-1" href="add-author.php">Add 
+Author</a></li> 
+<li role="presentation"><a role="menuitem" tabindex="-1" 
+href="manage-authors.php">Manage Authors</a></li> 
+</ul> 
+</li> 
+<li> 
+<a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown"> Books <i 
+class="fa fa-angle-down"></i></a> 
+<ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem"> 
+<li role="presentation"><a role="menuitem" tabindex="-1" href="add-book.php">Add 
+Book</a></li> 
+<li role="presentation"><a role="menuitem" tabindex="-1" href="manage
+Books</a></li> 
+</ul> 
+</li> 
+<li> 
+<a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown"> Issue 
+Books <i 
+New 
+class="fa fa-angle-down"></i></a> 
+<ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem"> 
+<li role="presentation"><a role="menuitem" tabindex="-1" href="issue-book.php">Issue 
+Book</a></li> 
+<li role="presentation"><a role="menuitem" tabindex="-1" 
+href="manage-issued-books.php">Manage Issued Books</a></li> 
+</ul> 
+</li> 
+<li><a href="reg-students.php">Reg Students</a></li> 
+<li><a href="change-password.php">Change Password</a></li> 
+</ul> 
+</div> 
+</div> 
+</div> 
+</div> 
+</section>
